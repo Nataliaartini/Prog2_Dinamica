9eba08da444ea67567dcf87fd1d72afee2b19b9c
Natalia Artini Ferrandin <79176834+Nataliaartini@users.noreply.github.com>
2022-05-16 20:31:43 -0300

    1sadsa

--- Tarefa.java
+++ Tarefa.java
@@ -6,15 +6,29 @@ public class Tarefa{
         Scanner scan = new Scanner(System.in);
         ArrayList<Integer> lista = new ArrayList();
 
-        for(int i=0; i<20; i++){
-            int numero = scan.nextInt();
-            lista.add(numero);
+        System.out.println("Digite 'N' quanto quiser parar.");
+        while(true){
+            try{
+                int numero = scan.nextInt();
+                lista.add(numero);
+            }
+            catch(Exception e){
+                break;
+            }
         }
 
         int maximo = Collections.max(lista);
         int minimo = Collections.min(lista);
 
+        double soma = 0;
+        for(int i = 0; i < lista.size(); i++)
+            soma += lista.get(i);
+
+        double media = (double)soma / lista.size(); 
+
+
         System.out.println("Maximo: " + maximo);
         System.out.println("Minimo: " + minimo);
+        System.out.println("Média: " + media);
     }
 }
